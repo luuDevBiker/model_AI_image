1e831baf6c8672746ce4dd436e24b231ed079c9a
luuDevBiker <luubiker@gmail.com>
2021-09-29 16:55:47 +0700

    commit main and packages

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-import cv2
-import model
+# import model
 path_foder = r'..\model_AI\img'
 array_path = os.listdir(path_foder)
 for i in range(len(array_path)):
     array_path[i] = path_foder +'\\'+ array_path[i]
-array_image = model.result_array_image(array_path)
-array_result = model.array_result(array_image)
+    print(array_path[i])
+# array_image = model.result_array_image(array_path)
+# array_result = model.array_result(array_image)
 # print(len(array_result))
 # for i in range(len(array_result)):
 #     print(array_result[i])
